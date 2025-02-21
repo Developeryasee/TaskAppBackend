9fea4bf8e9c9b48b4c81124517f1995e8a1b50a7
YasarArafath <yasar.arafath@thoughthub.net>
2025-02-21 08:29:14 +0530

    code changes

--- server.js
+++ server.js
@@ -28,4 +28,9 @@ app.use("/tasks",taskRoutes);
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Task API!");
+});
+
+
 module.exports = app;
